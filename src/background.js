edae3aba0bbc6c78c34804ed5f52aca82631e980
Kuip, Stefan (ELS-AMS) <chrome-0d9vd910@skuip.net>
2019-08-18 21:40:31 +0200

    When activated for a second time for fixed elements after a 2px scroll.

--- src/background.js
+++ src/background.js
@@ -1,9 +1,18 @@
-/* global chrome */
-
 function changeSettings (tab) {
-	chrome.tabs.sendMessage(tab.id, { action: `go` }, function () {
+	chrome.tabs.sendMessage(tab.id, { action: `go` }, function (response) {
+		if (response === `OK`) return console.log(tab.id, `Message received`);
+		// Message fails first time, since script isn`t yet injected.
 		if (chrome.runtime.lastError) {
-			chrome.tabs.executeScript(tab.id, {file: `content_script.js`});
+			console.log(tab.id, `Injecting content_script.js`);
+			chrome.tabs.executeScript(tab.id, {file: `content_script.js`}, function (response) {
+				if (response[0] !== `OK`) {
+					return console.warn(tab.id, `Unexpected script response`, response);
+				}
+				chrome.tabs.sendMessage(tab.id, { action: `go` }, function (response) {
+					if (response === `OK`) return console.log(tab.id, `Message received`);
+					console.warn(tab.id, `Unexpected message response`);
+				});
+			});
 		}
 	});
 }
--- src/content_script.js
+++ src/content_script.js
@@ -1,33 +1,99 @@
-/* global document, window */
-
 (function () {
-	var style = document.createElement(`style`);
+	let style = document.createElement(`style`);
 	style.textContent = `
+			body, html {
+				min-height: 101vh;
+			}
+			[data-sk-defixer]{
+				transition: all 1s;
+			}
 			[data-sk-defixer="true"]{
-				display: none !important;
 				opacity: 0! important;
-				visibility: hidden !important;
+				pointer-events: none !important;
 			}
 		`;
 	document.head.appendChild(style);
 
 	[``, `-x`, `-y`].forEach(function (style) {
-		document.body.style[`overflow` + style] = `auto`;
-		document.documentElement.style[`overflow` + style] = `auto`;
+		document.body.style[`overflow` + style] = `visible`;
+		document.documentElement.style[`overflow` + style] = `visible`;
 	});
 
 	chrome.runtime.onMessage.addListener(handleMessage);
 
-	function handleMessage () {
+	function handleMessage(request, sender, sendResponse) {
 		// Loop over every HTMLElement and determine if it`s position fixed.
-		var elements = document.querySelectorAll(`*`);
-		for (var i = elements.length - 1; i >= 0; i--) {
-			style = window.getComputedStyle(elements[i]);
-			if (style.position === `fixed`) {
-				elements[i].setAttribute(`data-sk-defixer`, `true`);
+		let elements = document.getElementsByTagName(`*`);
+
+		const state = parseInt(document.documentElement.dataset.skFixerState || 0, 10);
+
+		document.documentElement.dataset.skFixerState = state + 1;
+
+		if (state === 1) {
+			// The second time over we move the screen a bit to see what stuck
+			for (let i = elements.length - 1; i >= 0; i--) {
+				let element = elements[i];
+				const { height, left, top, width } = element.getBoundingClientRect();
+				if (height > 0 && width > 0) {
+					element.dataset.skDefixerRect = width.toFixed(0) + `x` + height.toFixed(0) + `+` + left.toFixed(0) + `+` + top.toFixed(0);
+				}
+			}
+
+			// Scroll the window a bit so we can detect *all* the fixed elements.
+			window.scrollBy(0,2);
+		}
+
+		if (state === 0 || state === 1) {
+			for (let i = elements.length - 1; i >= 0; i--) {
+				let element = elements[i];
+				let style = window.getComputedStyle(element);
+
+
+				if (style.position === `fixed`) {
+					element.setAttribute(`data-sk-defixer`, `true`);
+				}
+				else if (style.position === `sticky`) {
+					element.style.position = `relative`;
+				}
+				else if (state === 1) {
+					// Check if the element stayed in the same location in the viewport.
+					const { height, left, top, width } = element.getBoundingClientRect();
+					if (height > 0 && width > 0) {
+						if (element.dataset.skDefixerRect === width.toFixed(0) + `x` + height.toFixed(0) + `+` + left.toFixed(0) + `+` + top.toFixed(0)) {
+							element.dataset.skDefixer = `true`;
+						}
+					}
+				}
+
+				if (style.textAlign === `justify`) {
+					element.style.textAlign = `left`;
+				}
 			}
 		}
 
+		if (state === 2) {
+			// Resetting everything back to pristine state
+			for (let i = elements.length - 1; i >= 0; i--) {
+				let element = elements[i];
+				if (element.dataset.skDefixerRect) {
+					delete element.dataset.skDefixerRect;
+				}
+				if (element.dataset.skDefixer) {
+					element.dataset.skDefixer = `false`;
+				}
+			}
+			delete document.documentElement.dataset.skFixerState;
+		}
+
+		// Remove anchor ping's
+		elements = document.querySelectorAll(`a[ping]`);
+		for (let i = elements.length - 1; i >= 0; i--) {
+			elements.removeAttribute(`ping`);
+		}
+
+		// Send reply message back.
+		sendResponse(`OK`);
 	}
-	handleMessage();
 })();
+
+`OK`;
